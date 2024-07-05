3e320371969fd5b10c68362ca91d9c5b20e1b176
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2024-07-05 18:17:03 +0200

    Added simple sidebar, navbar

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+
+export const Navbar = () => {
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+      <div className="hidden lg:flex lg:flex-1" bg-yellow>
+        Search
+      </div>
+      <div className="">
+        <UserButton />
+      </div>
+    </div>
+  );
+};
---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+  return (
+    <aside className="fixed  h-[calc(100vh-0.5rem)] w-[60px] z-[1] top-1 bottom-1 bg-blue-800 text-white flex flex-col p-3 gap-y-4 rounded-l-md">
+      Sidebar!
+    </aside>
+  );
+};
