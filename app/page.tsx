566b226df40676f596cc12e698a537344c016bf5
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2024-07-05 17:31:46 +0200

    Convex + clerk setup with auth middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button size="sm" variant={"success"}>
-      Click me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for authenticated users</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
   );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://just-iguana-36.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
