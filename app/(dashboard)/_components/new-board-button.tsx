f7d60da6e4b2a8123402a042a04dbcaae4753fd3
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2024-07-31 11:48:27 +0200

    ~Fix~ Add redirect on creating new board

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -2,6 +2,7 @@
 
 import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
@@ -13,6 +14,7 @@ interface NewBoardButtonProps {
 }
 
 export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
+  const router = useRouter();
   const { mutate, pending } = useApiMutation(api.board.create);
 
   const onClick = async () => {
@@ -22,7 +24,7 @@ export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
     })
       .then((id) => {
         toast.success("Board created!");
-        //TODO: Redirect to the new board
+        router.push(`/board/${id}`);
       })
       .then(() => {
         toast.error("Failed to create board");
