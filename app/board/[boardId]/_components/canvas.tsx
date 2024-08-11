73ecdb0cd48869bbab5c7e66946d347331e89bec
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2024-08-11 20:43:50 +0200

    ~Planning~ Support for toolbox modes
    - Added type system for modes
    - Added undo and redo from liveblocks to canvas
    - Connect switching mode type to toolbox component

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { useState } from "react";
+
+import { useHistory, useCanRedo, useCanUndo } from "@liveblocks/react/suspense";
+import { CanvasMode, CanvasState } from "@/types/canvas";
+
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
@@ -9,11 +14,26 @@ interface CanvasProps {
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   return (
     <main className="h-full w-full relative touch-none bg-secondary">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        redo={() => {}}
+        undo={() => {}}
+      />
     </main>
   );
 };
