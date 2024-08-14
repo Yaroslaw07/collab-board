8b2c23a0aca7959c2731917410b36f39563c0cad
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2024-08-14 12:27:50 +0200

    ~Feature~ Added rectangles printing to canvas

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -3,34 +3,94 @@
 import React from "react";
 import { useCallback, useState } from "react";
 
+import { nanoid } from "nanoid";
+
 import {
   useHistory,
   useCanRedo,
   useCanUndo,
   useMutation,
+  useStorage,
 } from "@liveblocks/react/suspense";
+import { LiveObject } from "@liveblocks/client";
 
-import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
+import {
+  Camera,
+  CanvasMode,
+  CanvasState,
+  Color,
+  Layer,
+  LayerType,
+  Point,
+} from "@/types/canvas";
 import { parsePointerEventToCanvasPoint } from "@/lib/utils";
 
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
 import { CursorsPresence } from "./cursor-presence";
+import { LayerPreview } from "./layer-preview";
+
+const MAX_LAYERS = 100;
 
 interface CanvasProps {
   boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const layersIds = useStorage((root) => root.layerIds);
+
   const [canvasState, setCanvasState] = useState<CanvasState>({
     mode: CanvasMode.None,
   });
+
   const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+  const [lastUserColor, setLastUserColor] = useState<Color>({
+    r: 0,
+    g: 0,
+    b: 0,
+  });
 
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
+
+  const insertLayer = useMutation(
+    (
+      { storage, setMyPresence },
+      layerType:
+        | LayerType.Ellipse
+        | LayerType.Rectangle
+        | LayerType.Text
+        | LayerType.Note,
+      position: Point
+    ) => {
+      const liveLayers = storage.get("layers");
+
+      if (liveLayers.size >= MAX_LAYERS) {
+        return;
+      }
+
+      const liveLayersIds = storage.get("layerIds");
+      const layerId = nanoid();
+
+      const layer = new LiveObject({
+        type: layerType,
+        x: position.x,
+        y: position.y,
+        height: 100,
+        width: 100,
+        fill: lastUserColor,
+      });
+
+      liveLayersIds.push(layerId);
+      liveLayers.set(layerId, layer);
+
+      setMyPresence({ selection: [layerId] }, { addToHistory: true });
+      setCanvasState({ mode: CanvasMode.None });
+    },
+    [lastUserColor]
+  );
 
   const onWheel = useCallback((e: React.WheelEvent) => {
     console.log({
@@ -60,6 +120,23 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     setMyPresence({ cursor: null });
   }, []);
 
+  const onPointerUp = useMutation(
+    ({}, e) => {
+      const point = parsePointerEventToCanvasPoint(e, camera);
+
+      if (canvasState.mode === CanvasMode.Inserting) {
+        insertLayer(canvasState.layerType, point);
+      } else {
+        setCanvasState({
+          mode: CanvasMode.None,
+        });
+      }
+
+      history.resume();
+    },
+    [camera, canvasState, history, insertLayer]
+  );
+
   return (
     <main className="h-full w-full relative touch-none bg-secondary">
       <Info boardId={boardId} />
@@ -77,8 +154,17 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         onWheel={onWheel}
         onPointerMove={onPointerMove}
         onPointerLeave={onPointerLeave}
+        onPointerUp={onPointerUp}
       >
-        <g>
+        <g style={{ transform: `translate(${camera.x}px, ${camera.y}px)` }}>
+          {layersIds.map((layerId) => (
+            <LayerPreview
+              key={layerId}
+              id={layerId}
+              onLayerPointerDown={() => {}}
+              selectionColor={"#000"}
+            />
+          ))}
           <CursorsPresence />
         </g>
       </svg>
--- components/room.tsx
+++ components/room.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { ReactNode } from "react";
+
 import {
   LiveblocksProvider,
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
+import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
+
+import { Layer } from "@/types/canvas";
 
 interface RoomProps {
   children: ReactNode;
@@ -16,7 +20,14 @@ interface RoomProps {
 export function Room({ children, roomId, fallback }: RoomProps) {
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth" throttle={16}>
-      <RoomProvider id={roomId} initialPresence={{ cursor: null }}>
+      <RoomProvider
+        id={roomId}
+        initialPresence={{ cursor: null, selection: [] }}
+        initialStorage={{
+          layers: new LiveMap<string, LiveObject<Layer>>(),
+          layerIds: new LiveList([]),
+        }}
+      >
         <ClientSideSuspense fallback={fallback ?? <div>Loading…</div>}>
           {children}
         </ClientSideSuspense>
