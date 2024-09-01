34dbc0307a2d5d9f0c05fac92e672148a0e1d3f5
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2024-09-01 23:42:57 +0200

    ~Planning~ Edit pro modal

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -1,24 +1,46 @@
 "use client";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogDescription,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useProModal } from "@/store/use-pro-modal";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export const ProModal = () => {
   const { isOpen, onClose } = useProModal();
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Pro Modal</DialogTitle>
-        </DialogHeader>
-        <DialogDescription>This is a pro modal</DialogDescription>
+      <DialogContent className="max-w-[340px] p-0 overflow-hidden">
+        <div className="aspect-video relative flex items-center justify-center">
+          <Image src="/pro.svg" alt="Pro" className="object-fit" fill />
+        </div>
+        <div
+          className={cn(
+            "text-neutral-700 mx-auto space-y-6 p-6",
+            font.className
+          )}
+        >
+          <h2 className="font-medium text-lg">🚀 Upgrade to Pro today!</h2>
+          <div className="pl-3">
+            <ul className="text-[11px] space-y-1 list-disc">
+              <li>Unlimited boards</li>
+              <li>Unlimited tools</li>
+              <li>Unlimited organizations</li>
+              <li>Unlimited members</li>
+            </ul>
+          </div>
+          <Button size="sm" className="w-full">
+            Upgrade
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
